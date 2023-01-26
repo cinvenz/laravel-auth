bf37b6e1bfeed3c233bfb07348d6f1311de0c457
cinvenz <vincenzobortune@gmail.com>
2023-01-26 13:05:00 +0100

    insert post in admin/posts

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <h1>{{$post->title}}</h1>
         <img src="{{$post->image}}" alt="{{$post->title}}">
+
         <p>
             {{$post->content}}
         </p>
