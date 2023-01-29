43ae73e3d3b5669e7ed144cf918fe659320027e1
cinvenz <vincenzobortune@gmail.com>
2023-01-29 11:10:57 +0100

    fix upload_img in post controller update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+    private $validations = [
+        'slug'      => [
+            'required',
+            'string',
+            'max:100',
+        ],
+        'title'     => 'required|string|max:100',
+        'image'     => 'url|max:100',
+        'uploaded_img'  => 'image|max:1024',
+        'content'   => 'string',
+        'excerpt'   => 'string',
+    ];
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     *  @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -29,7 +43,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -40,7 +54,32 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $this->validations['slug'][] = 'unique:posts';
+        // $request->validate($this->validations);
+
+        $request->validate([
+            'slug'      => 'required|string|max:100|unique:posts',
+            'title'     => 'required|string|max:100',
+            'image'     => 'url|max:100',
+            'uploaded_img'  => 'image|max:1024',
+            'content'   => 'string',
+            'excerpt'   => 'string',
+        ]);
+
+        $data = $request->all();
+
+        $img_path = Storage::put('uploads', $data['uploaded_img']);
+
+        $post = new Post;
+        $post->slug          = $data['slug'];
+        $post->title         = $data['title'];
+        $post->image         = $data['image'];
+        $post->uploaded_img  = $img_path;
+        $post->content       = $data['content'];
+        $post->excerpt       = $data['excerpt'];
+        $post->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $post]);
     }
 
     /**
@@ -62,7 +101,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
@@ -74,7 +113,38 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        // $this->validations['slug'][] = Rule::unique('posts')->ignore($post);
+        // $request->validate($this->validations);
+
+        $request->validate([
+            'slug'      => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('posts')->ignore($post),
+            ],
+            'title'     => 'required|string|max:100',
+            'image'     => 'url|max:100',
+            'uploaded_img'  => 'image|max:1024',
+            'content'   => 'string',
+            'excerpt'   => 'string',
+        ]);
+
+
+        $data = $request->all();
+
+        $img_path = Storage::put('uploads', $data['uploaded_img']);
+        Storage::delete($post->uploaded_img);
+
+        $post->slug     = $data['slug'];
+        $post->title    = $data['title'];
+        $post->image    = $data['image'];
+        $post->uploaded_img  = $img_path;
+        $post->content  = $data['content'];
+        $post->excerpt  = $data['excerpt'];
+        $post->update();
+
+        return redirect()->route('admin.posts.show', ['post' => $post]);
     }
 
     /**
@@ -85,6 +155,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('admin.posts.index')->with('success_delete', $post);
     }
 }
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,11 +2,10 @@
 
 @section('content')
     <div class="container">
-        <h1>{{$post->title}}</h1>
-        <img src="{{$post->image}}" alt="{{$post->title}}">
-
+        <h1>{{ $post->title }}</h1>
+        <img src="{{ asset('storage/' . $post->uploaded_img) }}" alt="{{ $post->title }}">
         <p>
-            {{$post->content}}
+            {{ $post->content }}
         </p>
     </div>
 @endsection
